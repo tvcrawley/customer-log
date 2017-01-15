f915f6a35d5f2a6266c757957c987d1c779a22c7
Toni Langley <chibia2@gmail.com>
2017-01-15 13:40:38 -0500

    able to add customers to database

--- app.js
+++ app.js
@@ -6,4 +6,11 @@ function CustomerLogFunction($firebaseArray) {
     var vm = this;
     var ref = firebase.database().ref().child("customers");
     this.customers = $firebaseArray(ref);
+    this.newCustomer = {};
+
+    // add new customers to database
+    this.addCustomer = function() {
+        this.customers.$add(this.newCustomer)
+        this.newCustomer = {};
+    }
 }
