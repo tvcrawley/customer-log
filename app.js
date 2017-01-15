d8c1a57a38a882ff958338f947aad12b3705fa25
Toni Langley <chibia2@gmail.com>
2017-01-15 13:25:10 -0500

    able to display seed data

--- app.js
+++ app.js
@@ -3,5 +3,7 @@ angular
     .controller("CustomerLogController", ["$firebaseArray", CustomerLogFunction]);
 
 function CustomerLogFunction($firebaseArray) {
-
+    var vm = this;
+    var ref = firebase.database().ref().child("customers");
+    this.customers = $firebaseArray(ref);
 }
